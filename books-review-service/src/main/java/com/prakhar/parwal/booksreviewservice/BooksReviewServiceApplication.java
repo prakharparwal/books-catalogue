e8e69934bde596ae7488043a083db993afa2c595
prakharparwal <prakhar.parwal@gmail.com>
2020-01-07 21:38:58 +0530

    Books Catalogue > First submission > Books review service

---
+++ books-review-service/src/main/java/com/prakhar/parwal/booksreviewservice/BooksReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prakhar.parwal.booksreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksReviewServiceApplication.class, args);
+	}
+
+}
---
+++ books-review-service/src/test/java/com/prakhar/parwal/booksreviewservice/BooksReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakhar.parwal.booksreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
