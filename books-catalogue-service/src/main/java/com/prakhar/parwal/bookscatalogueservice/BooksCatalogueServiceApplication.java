24e46d56273d5086b722decf94d332518577d5bc
prakharparwal <prakhar.parwal@gmail.com>
2020-01-07 21:24:08 +0530

    Books Catalogue > First microservice > Books cataluge

---
+++ books-catalogue-service/src/main/java/com/prakhar/parwal/bookscatalogueservice/BooksCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prakhar.parwal.bookscatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksCatalogueServiceApplication.class, args);
+	}
+
+}
---
+++ books-catalogue-service/src/test/java/com/prakhar/parwal/bookscatalogueservice/BooksCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakhar.parwal.bookscatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
