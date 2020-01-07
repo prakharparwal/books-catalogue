2cd05e529a47bb279870a335eb78be7b5cc2f019
prakharparwal <prakhar.parwal@gmail.com>
2020-01-07 21:30:36 +0530

    Books Catalogure > First submission > Books details service

---
+++ books-details-service/src/main/java/com/prakhar/parwal/booksdetailsservice/BooksDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prakhar.parwal.booksdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksDetailsServiceApplication.class, args);
+	}
+
+}
