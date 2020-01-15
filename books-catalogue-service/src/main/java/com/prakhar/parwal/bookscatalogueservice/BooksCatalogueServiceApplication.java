fc84a9cb93a6987a5f7989bebfd555e656257e86
prakharparwal <prakhar.parwal@gmail.com>
2020-01-15 21:25:43 +0530

    Books Catalogue service, Books details service, books review service Created these services as Eureka Client

--- books-catalogue-service/src/main/java/com/prakhar/parwal/bookscatalogueservice/BooksCatalogueServiceApplication.java
+++ books-catalogue-service/src/main/java/com/prakhar/parwal/bookscatalogueservice/BooksCatalogueServiceApplication.java
@@ -2,11 +2,14 @@ package com.prakhar.parwal.bookscatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.ModelAndView;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan("com.prakhar.parwal.controllers.*")
 public class BooksCatalogueServiceApplication {
@@ -16,6 +19,7 @@ public class BooksCatalogueServiceApplication {
 	}
 	
 	@Bean 
+	@LoadBalanced
 	public WebClient.Builder getWebClientBuilder() {
 		return WebClient.builder();
 	}
